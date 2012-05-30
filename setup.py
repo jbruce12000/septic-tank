5fbc524a2e5424d40805283ee621333ad7f30770
jason <jason@upstairs.(none)>
2012-05-30 19:42:20 -0400

    - add a few comments for building on mint 12

--- setup.py
+++ setup.py
@@ -12,6 +12,11 @@ setup(
     url = "https://github.com/coxmediagroup/septic_tank",
 )
 
+#----------------------------------------------------------------------------
+# These are the commands I use to create a virtual environment in mint12.
+# Your mileage may vary.
+#----------------------------------------------------------------------------
+# sudo apt-get install -y build-essential python-virtualenv uuid-dev python-dev
 # virtualenv -p python2.7 vseptic_tank
 # source ./vseptic_tank/bin/activate
 # pip install -r requirements.txt
