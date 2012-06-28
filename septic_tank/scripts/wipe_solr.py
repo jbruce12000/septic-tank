7431def52c6fbb110c8274855c65f85cb63e4d85
Jason Bruce <jason.bruce@coxinc.com>
2012-06-28 08:01:15 -0400

    -comment about making web requests to do delete by query

--- septic_tank/scripts/wipe_solr.py
+++ septic_tank/scripts/wipe_solr.py
@@ -8,3 +8,6 @@ conn.delete(q='*:*')
 
 #curl "http://localhost:8983/solr/update?commit=true" -H "Content-Type: text/xml" 
 #   --data-binary "<delete><query>timestamp:[* TO NOW/DAY-30DAYS]</query></delete>
+
+# you can do this in a browser too
+#http://virtdev.cei.cox.com:8080/solr/medley/update?stream.body=%3Cdelete%3E%3Cquery%3Edate_dt:[*%20TO%20NOW-2DAYS]%3C/query%3E%3C/delete%3E&commit=true
